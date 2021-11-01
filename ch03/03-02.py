034229cc91fd8298c0ff965722bc9546d6ae2bf5
Yi Jinuk <jinuk.yi@twolinecode.com>
2021-11-01 18:25:01 +0900

    3장 추가

--- ch03/03-02.py
+++ ch03/03-02.py
@@ -3,16 +3,34 @@ import matplotlib.pylab as plt
 
 
 def step_function(x):
-    y = x > 0
-    return y.astype(int)
-    # return np.array(x > 0, dtype=int)
+    # y = x > 0
+    # return y.astype(int)
+    return np.array(x > 0, dtype=int)
 
+
+# x = np.arange(-5.0, 5.0, 0.1)
+# print(x)
+# y = step_function(x)
+# print(y)
+
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)  
+# plt.show()
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 x = np.arange(-5.0, 5.0, 0.1)
+# x = [n for n in range(-5, 6, 1)]
 print(x)
-y = step_function(x)
+
+y = sigmoid(x)
 print(y)
 
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)  
 plt.show()
+
+
+def relu(x):
+    return np.maximum(0, x)
