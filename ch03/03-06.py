f279ab1a692f0950c07cd454ed78efcc5b2f32cf
Yi Jinuk <jinuk.yi@twolinecode.com>
2021-11-02 17:42:41 +0900

    3장 추가

---
+++ ch03/03-06.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import pickle
+import numpy as np
+sys.path.append('/Volumes/DevHD/LearningProjects/DeepLearning/deeplearning_from_scratch')
+from dataset.mnist import load_mnist
+from common.functions import sigmoid, softmax
+
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(flatten=True, normalize=True, one_hot_label=False)
+    return x_test, t_test
+
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        # 학습된 가중치 매개변수가 담긴 파일
+        # 학습 없이 바로 추론을 수행
+        network = pickle.load(f)
+
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
